dbcc6fd03fe2897296e07a41514bfc4fbe881ebc
Muzammil Loya <73660510+Muzammil-cyber@users.noreply.github.com>
2023-05-31 16:55:02 +0500

    Name Update

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
           The Social App
         </h1>
         <p className="lead">
-          Join the Sucious revolution! Our app, powered by Motoko, is the social
+          Join the Socius revolution! Our app, powered by Motoko, is the social
           media platform of the future. Mint your own memes, send encrypted
           messages, and connect with friends worldwide. Sign up today and stay
           up-to-date in the ever-evolving world of social media.
